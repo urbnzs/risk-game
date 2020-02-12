ca503cbfdf6e02dcf14290b023008244412afe7d
Zsuzsanna Urban <urbanzsuzsii@gmail.com>
2020-02-12 14:22:36 +0100

    territory and surround moves

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@ let gameCells = document.querySelectorAll('.game-cell');
 let gameBoard = document.getElementById('game-board');
 let player = document.getElementById('game-board').getAttribute('data-player');
 let color = document.getElementById('game-board').getAttribute('data-color');
+let startRound = 0;
 
 function marker(coordinateX, coordinateY, activePlayer, activeColor) {
     for (let gameCell of gameCells) {
@@ -12,8 +13,8 @@ function marker(coordinateX, coordinateY, activePlayer, activeColor) {
             gameCell.setAttribute('color', activeColor)
         }
     }
-
 }
+
 
 socket.on('connect', function () {
     socket.emit('start')
@@ -25,18 +26,22 @@ function clickHandler(t) {
         let clickedTarget = t.target;
         let xcoord = clickedTarget.getAttribute('data-coordinate-x');
         let ycoord = clickedTarget.getAttribute('data-coordinate-y');
-        socket.emit('attack', {
-            xcoord: xcoord,
-            ycoord: ycoord,
-            activePlayer: player,
-            activeColor: color
+        let cellOwner = clickedTarget.getAttribute('data-owner')
+        console.log(surroundedChecker())
+        if (startingRound() || nextCellChecker(xcoord, ycoord, cellOwner) || surroundedChecker()) {
+            socket.emit('attack', {
+                xcoord: xcoord,
+                ycoord: ycoord,
+                activePlayer: player,
+                activeColor: color
 
-        })
+            })
+
+            gameBoard.removeEventListener('click', clickHandler);
+            socket.emit('next player', player);
+        }
+        }
     }
-    gameBoard.removeEventListener('click', clickHandler);
-    socket.emit('next player', player);
-
-}
 
 
 socket.on('stream attack', function (data) {
@@ -50,3 +55,162 @@ socket.on('start game', function (activePlayer) {
         gameBoard.addEventListener('click', clickHandler);
     }
 });
+
+function startingRound() {
+    if (startRound < 2) {
+        startRound ++
+    }
+    if (startRound <= 1) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+function nextCellChecker(xCoord, yCoord, cellOwner) {
+    let upperCell = 0
+    for (let gameCell of gameCells) {
+        if (parseInt(yCoord) - 1 === parseInt(gameCell.dataset.coordinateY) && xCoord === gameCell.dataset.coordinateX) {
+            upperCell = gameCell
+        }
+    }
+
+
+    let underCell = 0
+    for (let gameCell of gameCells) {
+        if (parseInt(yCoord) + 1 === parseInt(gameCell.dataset.coordinateY) && xCoord === gameCell.dataset.coordinateX) {
+            underCell = gameCell
+        }
+    }
+
+    let rightCell = 0
+    for (let gameCell of gameCells) {
+        if (yCoord === gameCell.dataset.coordinateY && parseInt(xCoord) + 1  === parseInt(gameCell.dataset.coordinateX)) {
+            rightCell = gameCell
+        }
+    }
+
+    let leftCell = 0
+    for (let gameCell of gameCells) {
+        if (yCoord === gameCell.dataset.coordinateY && parseInt(xCoord) - 1  === parseInt(gameCell.dataset.coordinateX)) {
+            leftCell = gameCell
+        }
+    }
+
+    let leftUpperCell = 0
+    for (let gameCell of gameCells) {
+        if (parseInt(yCoord) + 1 === parseInt(gameCell.dataset.coordinateY)
+            && parseInt(xCoord) - 1  === parseInt(gameCell.dataset.coordinateX)) {
+            leftUpperCell = gameCell
+        }
+    }
+
+    let rightUpperCell = 0
+    for (let gameCell of gameCells) {
+        if (parseInt(yCoord) + 1 === parseInt(gameCell.dataset.coordinateY)
+            && parseInt(xCoord) + 1  === parseInt(gameCell.dataset.coordinateX)) {
+            rightUpperCell = gameCell
+        }
+    }
+
+    let leftUnderCell = 0
+    for (let gameCell of gameCells) {
+        if (parseInt(yCoord) - 1 === parseInt(gameCell.dataset.coordinateY)
+            && parseInt(xCoord) - 1  === parseInt(gameCell.dataset.coordinateX)) {
+            leftUnderCell = gameCell
+        }
+    }
+
+    let rightUnderCell = 0
+    for (let gameCell of gameCells) {
+        if (parseInt(yCoord) - 1 === parseInt(gameCell.dataset.coordinateY)
+            && parseInt(xCoord) + 1  === parseInt(gameCell.dataset.coordinateX)) {
+            rightUnderCell = gameCell
+        }
+    }
+
+    let result = false
+
+    if (cellOwner === 'None' && upperCell !== 0) {
+        console.log('upper cell nem üres')
+        if (upperCell.dataset.owner === player) {
+            result = true
+        }
+    }
+
+    if (cellOwner === 'None' && underCell !== 0) {
+        if (underCell.dataset.owner === player) {
+            result = true
+        }
+    }
+    if (cellOwner === 'None' && rightCell !== 0) {
+        if (rightCell.dataset.owner === player) {
+            result = true
+        }
+    }
+    if (cellOwner === 'None' && leftCell !== 0) {
+        if (leftCell.dataset.owner === player) {
+            result = true
+        }
+    }
+    if (cellOwner === 'None' && leftUpperCell !== 0) {
+        if (leftUpperCell.dataset.owner === player) {
+            result = true
+        }
+    }
+
+    if (cellOwner === 'None' && rightUpperCell !== 0) {
+        if (rightUpperCell.dataset.owner === player) {
+            result = true
+        }
+    }
+
+    if (cellOwner === 'None' && leftUnderCell !== 0) {
+        if (leftUnderCell.dataset.owner === player) {
+            result = true
+        }
+    }
+
+    if (cellOwner === 'None' && rightUnderCell !== 0) {
+        if (rightUnderCell.dataset.owner === player) {
+            result = true
+        }
+    }
+
+    return result
+
+}
+
+
+function surroundedChecker() {
+    let emptyNextCells = 0
+    for (let gameCell of gameCells) {
+        if (nextCellChecker(gameCell.dataset.coordinateX, gameCell.dataset.coordinateY, gameCell.dataset.owner)) {
+            emptyNextCells ++
+        }
+    }
+    console.log(emptyNextCells)
+    if (emptyNextCells > 0) {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
